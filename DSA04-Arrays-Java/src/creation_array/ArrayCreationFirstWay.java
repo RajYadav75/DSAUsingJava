ab7391afb6836c56c526ddb9afdf1860deb6a45b
RajYadav75 <rajyadav751811@gmail.com>
2025-11-05 09:15:41 +0530

    Array Creation Implementation

---
+++ DSA04-Arrays-Java/src/creation_array/ArrayCreationFirstWay.java
@@ -0,0 +1,17 @@
+package creation_array;
+
+public class ArrayCreationFirstWay {
+    public static void main(String[] args) {
+        int[] arr; //
+        arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        System.out.println(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ DSA04-Arrays-Java/src/creation_array/ArrayCreationSecondWay.java
@@ -0,0 +1,11 @@
+package creation_array;
+
+public class ArrayCreationSecondWay {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(arr);
+        for(int array:arr){
+            System.out.println(array);
+        }
+    }
+}
