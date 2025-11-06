3043e387148d61bda97171b6a98fbc3368b7121d
RajYadav75 <rajyadav751811@gmail.com>
2025-11-06 09:25:04 +0530

    Array Printing Implementation

---
+++ DSA04-Arrays-Java/src/printing_array/ArrayPrint.java
@@ -0,0 +1,20 @@
+package printing_array;
+
+public class ArrayPrint {
+    public static void main(String[] args) {
+        int[] arr; //
+        arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        System.out.println("This is address of Array "+arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        for(int array:arr){
+            System.out.println(array);
+        }
+    }
+}
