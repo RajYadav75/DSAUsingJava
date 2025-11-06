e18fb4efdecea1d8d3f04911ab1b9bdaf393876b
RajYadav75 <rajyadav751811@gmail.com>
2025-11-06 23:08:09 +0530

    Array Creation Implementation

---
+++ DSA04-Arrays-Java/src/creation_array/ArrayCreationThirdWay.java
@@ -0,0 +1,9 @@
+package creation_array;
+
+public class ArrayCreationThirdWay {
+    public static void main(String[] args) {
+        int[] arr;
+
+        arr = new int[]{1,2,3,4,5};
+    }
+}
---
+++ DSA04-Arrays-Java/src/creation_array/MordernJavaEightArrayCreationFourWay.java
@@ -0,0 +1,11 @@
+package creation_array;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MordernJavaEightArrayCreationFourWay {
+    public static void main(String[] args) {
+        //TODO:- Creates an array [1, 2, 3, 4, 5]
+        int[] arr = IntStream.range(1, 6).toArray();
+    }
+}
