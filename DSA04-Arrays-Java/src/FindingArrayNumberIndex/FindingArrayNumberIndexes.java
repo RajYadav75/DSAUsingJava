b5411fdde20ca97941505b0efe010787f96f245c
RajYadav75 <rajyadav751811@gmail.com>
2025-11-14 23:04:22 +0530

    Linear Search Implementation

--- DSA04-Arrays-Java/src/FindingArrayNumberIndex/FindingArrayNumberIndexes.java
+++ DSA04-Arrays-Java/src/FindingArrayNumberIndex/FindingArrayNumberIndexes.java
@@ -1,4 +1,19 @@
 package FindingArrayNumberIndex;
 
 public class FindingArrayNumberIndexes {
+
+    public static void main(String[] args) {
+        int[] arr = {71, 22, 93, 54, 85, 96, 107, 88, 99};
+        int findingValue = 107;
+        System.out.println(findMaxNumberIndex(arr, findingValue));
+    }
+
+    private static int findMaxNumberIndex(int[] arr, int findingValue) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == findingValue) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
